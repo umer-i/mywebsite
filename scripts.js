9189cabb875325816129db492c6211c60910b6da
umer-i <79803255+umer-i@users.noreply.github.com>
2024-06-01 12:54:07 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,28 +1,44 @@
 // Define the toggleSection function
 function toggleSection(sectionId) {
-    const section = document.getElementById(sectionId);
-    const content = section.querySelector('.section-content');
-    const isOpen = content.style.maxHeight;
-
-    if (isOpen) {
-        content.style.maxHeight = null;
-    } else {
-        content.style.maxHeight = content.scrollHeight + 'px';
-    }
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+        const content = section.querySelector('.section-content');
+        const notch = section.querySelector('.section-notch');
+        if (section.id === sectionId) {
+            const isOpen = section.classList.contains('section-open');
+            if (isOpen) {
+                content.style.maxHeight = null;
+                section.classList.remove('section-open');
+            } else {
+                content.style.maxHeight = content.scrollHeight + 'px';
+                section.classList.add('section-open');
+            }
+        } else {
+            content.style.maxHeight = null;
+            section.classList.remove('section-open');
+        }
+    });
 }
 
 // Fade in the main title and add secondary text after 3 seconds
 document.addEventListener("DOMContentLoaded", () => {
     const mainTitle = document.getElementById('main-title');
     mainTitle.style.opacity = '0';
-    mainTitle.style.transition = 'opacity 1.5s';
+    mainTitle.style.transition = 'opacity 1.5s, transform 1.5s';
     mainTitle.style.opacity = '1';
+    mainTitle.style.transform = 'translateY(0)';
 
     setTimeout(() => {
         const subtext = document.getElementById('subtext');
         subtext.classList.remove('hidden');
         subtext.style.opacity = '0';
-        subtext.style.transition = 'opacity 1.5s';
+        subtext.style.transition = 'opacity 1.5s, transform 1.5s';
         subtext.style.opacity = '1';
+        subtext.style.transform = 'translateY(0)';
     }, 3000);
+});
+
+// Toggle dark and light mode
+document.getElementById('theme-toggle').addEventListener('change', (event) => {
+    document.body.classList.toggle('dark-mode', event.target.checked);
 });
